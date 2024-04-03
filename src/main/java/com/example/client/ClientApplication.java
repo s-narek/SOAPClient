6c9684b6417145a6f426d51b88eab6617611f160
treedorg-dp <treedorg@deeplay.io>
2024-04-03 15:16:41 +0300

    Исправления

--- src/main/java/com/example/client/ClientApplication.java
+++ src/main/java/com/example/client/ClientApplication.java
@@ -2,8 +2,6 @@ package com.example.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.xmlsoap.schemas.soap.envelope.Auth;
-import org.xmlsoap.schemas.soap.envelope.GetOutMessageRequest;
 
 @SpringBootApplication
 public class ClientApplication {
